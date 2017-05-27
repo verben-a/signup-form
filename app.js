0fa6dc688491c1a7cf65e55d961c0f250450ac97
verben-a <alina.verbenchuk@gmail.com>
2017-05-27 23:04:48 +0100

    Initial commit

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ app.directive('signUp', function() {
 	return {
 		restrict: 'E',
 		require: '^ngModel',
-		template: '<div class="alina">Alina</div>'
+		template: '<div class="form"><div signup></div><input type="text" ng-model="name" placeholder="First Name"/><input type="text" ng-model="surname" placeholder="Last Name"/><br><input type="text" ng-model="email" placeholder="Email"/><br><br><button class="submit">Give us your infoz!</button></div>'
 	}
-})
-.controller('someCtrl', function(){
-
 })
